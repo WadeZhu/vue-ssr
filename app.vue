b3b7155abee163797b3bcec25cc12d505df30efa
祝纬 <shuku@zhuweideMacBook-Pro.local>
2018-05-17 09:02:38 +0800

    init

---
+++ app.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id='app' style='height: 100%;'>
+        Helloweeen
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+    components: {
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+body {
+    background-color: #f0f8fa;
+    overflow: hidden;
+    background-repeat: no-repeat;
+    background-position: center bottom;
+    background-size: contain;
+
+}
+
+#app {
+    height: 100%;
+    font-family: MicrosoftYaHei;
+}
+
+.clearfloat:after {
+    content: '';
+    display: block;
+    clear: both;
+    visibility: hidden;
+    height: 0
+} 
+
+.clearfloat {
+    zoom: 1;
+} 
+</style>
+
---
+++ server.js
@@ -0,0 +1,33 @@
+const Vue = require('vue')
+const server = require('express')()
+const renderer = require('vue-server-renderer').createRenderer({
+    template: require('fs').readFileSync('./templete.html', 'utf-8')
+})
+
+
+server.get('*', (req, res) => {
+    const app = new Vue({
+        data: {
+            url: req.url
+        },
+        template: `<div>访问的 URL 是： {{ url }}</div>`
+    })
+
+    const context = {
+        title: 'hello',
+        meta: ''
+    }
+
+    renderer.renderToString(app, context, (err, html) => {
+        if (err) {
+            res.status(500).end('Internal Server Error')
+            return
+        }
+
+        res.end(html)
+    })
+})
+
+server.listen(8080)
+
+console.log('I am listening 8080')
